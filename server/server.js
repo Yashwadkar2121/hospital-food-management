6c519873802ff2e8d220e952bbe899d456350701
Yash Wadkar <yashwadkar079@gmail.com>
2025-01-08 09:01:58 +0530

    Creating React app and Cors policy setup

--- server/server.js
+++ server/server.js
@@ -7,11 +7,33 @@ const pantryRoutes = require("./routes/pantryRoutes");
 const deliveryRoutes = require("./routes/deliveryRoutes");
 const userRoutes = require("./routes/userRoutes");
 
+// Database function
 connectToMongo();
 
+// Express app
 const app = express();
 app.use(express.json());
 
+// Cors policy
+const cors = require("cors");
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+// Available Routes
 app.use("/api/patients", patientRoutes);
 app.use("/api/dietcharts", dietChartRoutes);
 app.use("/api/pantry", pantryRoutes);
